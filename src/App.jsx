d8e2865aba8c386d8d6b0500d329237795a1ee2c
tobolovskaya <142045878+tobolovskaya@users.noreply.github.com>
2024-10-10 22:02:26 +0200

    1

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,13 @@ import styles from './App.module.css';
 const App = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Contact Book</h1>
+      <h1 className={styles.title}>Phonebook</h1>
       
       <section className={styles.section}>
-        <h2 className={styles.subtitle}>Add a new contact</h2>
+        <h2 className={styles.subtitle}>Add New Contact</h2>
         <ContactsForm />
       </section>
-      
+
       <section className={styles.section}>
         <h2 className={styles.subtitle}>Contacts</h2>
         <SearchBox />
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
+import App from './App';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
+);
