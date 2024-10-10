d331fb1205363bb7be11e2f21a85682cd3a596eb
tobolovskaya <142045878+tobolovskaya@users.noreply.github.com>
2024-10-10 22:51:01 +0200

    1

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,21 +3,22 @@ import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import contactsReducer from './contactsSlice';
 import filtersReducer from './filtersSlice';
+import { combineReducers } from 'redux';
 
+// Налаштування персистенції
 const persistConfig = {
   key: 'contacts',
   storage,
-  whitelist: ['items'],
+  whitelist: ['items'], // Тільки items з контактів буде збережено
 };
 
-const persistedContactsReducer = persistReducer(persistConfig, contactsReducer);
+const rootReducer = combineReducers({
+  contacts: persistReducer(persistConfig, contactsReducer),
+  filters: filtersReducer,
+});
 
-const store = configureStore({
-  reducer: {
-    contacts: persistedContactsReducer,
-    filters: filtersReducer,
-  },
+export const store = configureStore({
+  reducer: rootReducer,
 });
 
 export const persistor = persistStore(store);
-export default store;
